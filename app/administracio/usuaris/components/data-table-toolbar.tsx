dd3480298023dc3bf32962c87c9bc8977076892f
Marc Perez de Tudela <mperezdetudelaaznar@gmail.com>
2025-08-26 15:56:00 +0200

    feat: add tooltip functionality to search input in data table toolbar

--- app/administracio/usuaris/components/data-table-toolbar.tsx
+++ app/administracio/usuaris/components/data-table-toolbar.tsx
@@ -9,6 +9,12 @@ import { DataTableViewOptions } from "./data-table-view-options";
 
 import { rols, accessos } from "../data/data";
 import { DataTableFacetedFilter } from "./data-table-faceted-filter";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface DataTableToolbarProps<TData> {
   table: Table<TData>;
@@ -22,12 +28,21 @@ export function DataTableToolbar<TData>({
   return (
     <div className="flex items-end justify-between flex-wrap gap-2 md:gap-0">
       <div className="flex flex-1 items-center gap-2 flex-wrap">
-        <Input
-          placeholder="Cercar..."
-          value={table.getState().globalFilter ?? ""}
-          onChange={(event) => table.setGlobalFilter(event.target.value)}
-          className="h-8 w-[150px] lg:w-[250px]"
-        />
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Input
+                placeholder="Cercar..."
+                value={table.getState().globalFilter ?? ""}
+                onChange={(event) => table.setGlobalFilter(event.target.value)}
+                className="h-8 w-[150px] lg:w-[250px]"
+              />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Cerca per nom, email o rol</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
         {table.getColumn("access") && (
           <DataTableFacetedFilter
             column={table.getColumn("access")}
